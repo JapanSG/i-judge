82ca00c927f47379164da0fb798e644b523b53ed
JapanSG <japan.sri45326@gmail.com>
2024-10-01 23:53:42 +0700

    mod distribute.py

--- Mock2/distribute.py
+++ Mock2/distribute.py
@@ -1,22 +1,27 @@
 '''distribute'''
+def eight(money:int,children:int) -> int:
+    '''find how many child get 8'''
+    if children > money or (money == 4 and children == 1):
+        return -1
+    if money < 8:
+        return 0
+    times = money//8
+    left = money%8
+    if times > children or (times == children and left > 0):
+        return children-1
+    if left == 4 and children-times == 1:
+        return times-1
+    if left < children - times: ## 25 testcase issue here children aprox(8.795*10**23)
+        temp = ((children-times)-left)//7
+        if ((children-times)-left)%7:
+            temp += 1
+        times -= temp
+    return int(times)
 def main():
     '''Driver Code'''
     money = int(input())
     children = int(input())
-    times = money//8
-    money_used = times*8
-    leftover = money%8
-    i = 1
-    if money_used > money or children-times > leftover:
-        while money_used > money or children-times > leftover:
-            money_used = (times-i)*8
-            leftover = money-money_used
-            i += 1
-            if leftover > money:
-                print(-1)
-                return
-        times -= i
-    if leftover - (children-times) == 3 and children-times <= 1:
-        times -= 1
-    print(times)
+    print(eight(money,children))
+# for i in range(1,107):
+#     print(f"money = {106} children = {i} ans = {eight(106,i)}")
 main()
